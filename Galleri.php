560b495f14702fe10e73a16c378ab15accfc0109
Joachim <joachim.berner@hotmail.com>
2022-02-09 10:02:27 +0100

    har ændret Hero-image sektionen så vi får en knap til at scolle ned til vores content.

--- Galleri.php
+++ Galleri.php
@@ -11,7 +11,7 @@
 
     <!-- Titel som ses oppe i browserens tab mv. -->
     <title>BMS - Galleri</title>
-    <link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico" />
+    <link rel="shortcut icon" type="image/x-icon" href="image/favicon.ico"/>
 
     <!-- Metatags der fortæller at søgemaskiner er velkomne, hvem der udgiver siden og copyright information -->
     <meta name="robots" content="All">
@@ -32,98 +32,115 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
-<header>
 
-    <h1 class="heroTekstTjenester">Galleri</h1>
-
-</header>
-
-<?php include 'navbar.php';?>
+<?php include 'navbar.php'; ?>
 
 
-<div class="gallery p-5" id="gallery">
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,care" alt=""></div>
+<div class="hero-full-screen">
+
+    <div class="top-content-section"></div>
+
+    <div class="middle-content-section">
+        <h1>Galleri Placeholder</h1>
     </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,studied" alt=""></div>
+
+    <div class="bottom-content-section" data-magellan data-threshold="0">
+        <a href="#main-content-section">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M24 12c0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12 12-5.373 12-12zm-18.005-1.568l1.415-1.414 4.59 4.574 4.579-4.574 1.416 1.414-5.995 5.988-6.005-5.988z"/>
+            </svg>
+        </a>
     </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,substance" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,choose" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,past" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,lamp" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,yet" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,eight" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,crew" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,event" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,instrument" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,practical" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,pass" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,bigger" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,number" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,feature" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,line" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,railroad" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,pride" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,too" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,bottle" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,base" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,cell" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,bag" alt=""></div>
-    </div>
-    <div class="gallery-item">
-        <div class="content"><img src="https://source.unsplash.com/random/?tech,card" alt=""></div>
+
+</div>
+
+<div id="main-content-section" data-magellan-target="main-content-section">
+
+
+
+    <div class="gallery p-5" id="gallery">
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,care" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,studied" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,substance" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,choose" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,past" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,lamp" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,yet" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,eight" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,crew" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,event" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,instrument" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,practical" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,pass" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,bigger" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,number" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,feature" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,line" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,railroad" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,pride" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,too" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,bottle" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,base" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,cell" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,bag" alt=""></div>
+        </div>
+        <div class="gallery-item">
+            <div class="content"><img src="https://source.unsplash.com/random/?tech,card" alt=""></div>
+        </div>
     </div>
 </div>
 
+<?php include 'footer.php'; ?>
 
-<?php include 'footer.php';?>
 
-
-<script src="./js/Galleri_Test2.js"></script>
+<script src="js/Galleri.js"></script>
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
--- fliserens.php
+++ fliserens.php
@@ -31,12 +31,29 @@
 <!-- i <body> har man alt indhold på siden som brugeren kan se -->
 <body>
 
-<?php include 'Nav_test.php';?>
+<?php include 'navbar.php';?>
+
+<div class="hero-full-screen">
+
+    <div class="top-content-section"></div>
+
+    <div class="middle-content-section">
+        <h1>Fliserens Placeholder</h1>
+    </div>
+
+    <div class="bottom-content-section" data-magellan data-threshold="0">
+        <a href="#main-content-section">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M24 12c0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12 12-5.373 12-12zm-18.005-1.568l1.415-1.414 4.59 4.574 4.579-4.574 1.416 1.414-5.995 5.988-6.005-5.988z"/>
+            </svg>
+        </a>
+    </div>
+
+</div>
 
 
 
-
-
+<div id="main-content-section" data-magellan-target="main-content-section">
 
 <div class="omcon container p-4 mb-3 mb-lg-5">
     <div class="row mt-5">
@@ -97,6 +114,8 @@
         </div>
 
     </div>
+</div>
+
 </div>
 
 <?php include 'footer.php';?>
