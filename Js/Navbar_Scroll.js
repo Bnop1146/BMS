fb6e9be28a1dab418ddc868ce1bd527ac8b4aaca
omertekbas <tekbas425@gmail.com>
2022-02-09 10:02:06 +0100

    .

--- Js/Navbar_Scroll.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('header');
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('scrolling-active', windowPosition);
-})
