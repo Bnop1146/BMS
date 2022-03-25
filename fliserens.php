49ffe87dd76e3983656f52a58f907fc57af822f6
Joachim <joachim.berner@hotmail.com>
2022-03-25 10:05:35 +0100

    Vi har udfyldt kundeanmeldelser

--- fliserens.php
+++ fliserens.php
@@ -46,7 +46,7 @@
 
     <div class="bottom-content-section" data-magellan data-threshold="0">
         <a href="#main-content-section">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 30 30">
                 <path d="M24 12c0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12 12-5.373 12-12zm-18.005-1.568l1.415-1.414 4.59 4.574 4.579-4.574 1.416 1.414-5.995 5.988-6.005-5.988z"/>
             </svg>
         </a>
@@ -98,7 +98,7 @@
         <div class="row">
 
             <div class="om2sek col-sm-12 col-md-6 col-lg-6" data-aos="fade-down">
-                <img src="image/video-placeholder.png" class="om2billede img-fluid" alt="">
+                <img src="image/fliserens-1.jpg" class="om2billede img-fluid" alt="">
             </div>
 
             <div class="om1sek col-sm-12 col-md-6 col-lg-6 " data-aos="fade-down">
